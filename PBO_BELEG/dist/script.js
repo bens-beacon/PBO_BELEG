9b3d5c81ae59347d71b69c5c9fb93280c7815365
Ben <benjosua@live.de>
2018-01-10 18:32:15 +0100

    edit SEARCH

--- PBO_BELEG/dist/script.js
+++ PBO_BELEG/dist/script.js
@@ -151,11 +151,14 @@
                 var tmp_text = event.target.value.toUpperCase();
                 
                 // search for state
-                //this.json_tmp = this.json_tmp.filter(function (n) { if (n.participation.toUpperCase().includes(tmp_text)) return n.participation;});
+                json_tmp_state = this.json_tmp.filter(function (n) { if (n.participation.toUpperCase().includes(tmp_text)) return n.participation;});
                 
                 // serach for project
-                //this.json_tmp = this.json_tmp.filter(function (n) { if (n.name.toUpperCase().includes(tmp_text)) return n.name;});
+                json_tmp_project = this.json_tmp.filter(function (n) { if (n.name.toUpperCase().includes(tmp_text)) return n.name;});
                 
+                this.json_tmp = json_tmp_project
+
+
                 // search for location
                 //var tmp = this.json_data.process.locations; 
                 //var loc_id = tmp.filter(function (n) { if (n.city.toUpperCase().includes(tmp_text)) return n.id;});
@@ -167,12 +170,15 @@
                 var tmp = this.json_data.process.stakeholder; 
                 var sta_id = tmp.filter(function (n) { if (n.name.toUpperCase().includes(tmp_text)) return n.id;});
                 //console.log(this.json_tmp[0].initiator);
+                /*
                 if(sta_id.length > 0){
                     this.json_tmp = this.json_tmp.filter(function (n) { 
                     for(i in sta_id){
                         if (n.initiator == sta_id[i].id) return n.initiator; }});
                         console.log(sta_id[i].id);
                 }
+                */
+
 
                 //SetRadius(this.json_tmp.length);
                 // if there is nothing to search
